be1a4c236381737870d74a7ec9f853e3a5dddc94
LAPTOP-N6HJER3M\mengchu <mengchu35@gmail.com>
2021-04-23 08:45:57 +0800

    [practice] BMI

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <BMI msg="BMI"/>
+  </div>
+</template>
+
+<script>
+import BMI from './components/BMI.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BMI
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BMI.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <form>
+      <h2><u>計算 BMI</u></h2>
+      <label for="height">
+        身高:&nbsp;
+        <input type="text" name="height" v-model.number="height" value="">
+        &nbsp;公分
+      </label>
+      <br><br>
+      <label for="weight">
+        體重:&nbsp;
+        <input type="text" name="weight" v-model.number="weight" value="">
+        &nbsp;公斤
+      </label>
+      <br><br>
+      <button v-on:click.prevent="submit()">Submit</button>
+      <p>{{bmi_value}}</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BMI',
+  data() {
+    return {
+      height: '',
+      weight: '',
+      bmi_value: ''
+    }
+  },
+  methods: {
+    submit() {
+      if (this.height != '' && this.weight != '') {
+        let bmi = this.weight / (this.height / 100) ** 2;
+        if (!isNaN(bmi)) {
+          this.bmi_value = bmi;
+        } else {
+          this.bmi_value = '請輸入數值 !';
+        }
+      } else {
+        this.bmi_value = '請輸入身高和體重 !';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  color: #8494A4;
+}
+
+p {
+  color: #C21414;
+}
+</style>
